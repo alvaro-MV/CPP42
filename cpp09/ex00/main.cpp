95d52a1a885bc1aac4a49cb93a06ba2ad16f7917
Álvaro Morales <moralesalvaro97@gmail.com>
2025-07-04 00:56:09 +0200

    Segmentation fault en cpp09/ex00, no se porque primera logica implementada.

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -4,10 +4,12 @@
 #include <string>
 #include <map>
 
-int	main(void) {
+typedef std::map<std::string, std::string> database;
+
+void	fill_database(database &db) {
 	std::string	row;
 	std::fstream fin;
-	std::map<std::string, std::string> database;
+	
 	fin.open("data.csv", std::ios::in);
 	
 	std::getline(fin, row);
@@ -18,9 +20,62 @@ int	main(void) {
 		std::getline(sstream, cols, ',');
 		std::string col1 = cols;
 		std::getline(sstream, cols, ',');
-		database[col1] = cols;
-		std::cout << database.find(col1)->first << ": " << database.find(col1)->second << std::endl;
+		db[col1] = cols;
+		// std::cout << db.find(col1)->first << ": " << db.find(col1)->second << std::endl;
 	}
-	
+}
 
+std::string remove_extra_spaces(const std::string& input) {
+    std::stringstream ss(input);
+    std::string word, result;
+
+    while (ss >> word) {
+        if (!result.empty())
+            result += " ";
+        result += word;
+    }
+
+    return result;
+}
+
+int	itoa(std::string str, bool &fail) {
+	int num;
+	std::istringstream iss(str);
+
+    iss >> num;
+	if (iss.fail() == false)
+	{
+		fail = false;
+		return (num);
+	}
+	else
+	{
+		fail = true;
+		return (-1);
+	}
+}
+
+int	main(void) {
+
+	database db;
+	fill_database(db);
+
+	std::string	row;
+	std::fstream fin;
+	fin.open("input.txt", std::ios::in);
+	
+	std::getline(fin, row);
+	while (std::getline(fin, row)) {
+		std::string cols;
+		std::stringstream sstream(row);
+
+		std::getline(sstream, cols, '|');
+		std::string col1 = cols;
+		col1 = remove_extra_spaces(col1);
+		std::cout << col1 << std::endl;
+		std::getline(sstream, cols, '|');
+		cols = remove_extra_spaces(cols);
+		// bool	fail;
+		std::cout << db.find(col1)->first <<std::endl; //<< ": " << itoa(db.find(col1)->second, fail) * itoa(cols, fail);
+	}
 }
