e450d14c73428239c95e1587405491f7b3af06b8
Álvaro Morales <moralesalvaro97@gmail.com>
2025-07-04 15:39:36 +0200

    When no number date exist in the database, then the result is not printed. Needed to cnovert to float.

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -20,9 +20,10 @@ void	fill_database(database &db) {
 		std::getline(sstream, cols, ',');
 		std::string col1 = cols;
 		std::getline(sstream, cols, ',');
-		db[col1] = cols;
+		db.insert(std::pair<std::string, std::string>(col1, cols));
 		// std::cout << db.find(col1)->first << ": " << db.find(col1)->second << std::endl;
 	}
+	std::cout << "Este es la de 2022-12-22: " << db.find("2021-12-22")->second << std::endl;
 }
 
 std::string remove_extra_spaces(const std::string& input) {
@@ -59,6 +60,7 @@ int	main(void) {
 
 	database db;
 	fill_database(db);
+	std::cout << "Por segunda vez de 2022-12-22: " << db.find("2021-12-22")->second << std::endl;
 
 	std::string	row;
 	std::fstream fin;
@@ -72,10 +74,13 @@ int	main(void) {
 		std::getline(sstream, cols, '|');
 		std::string col1 = cols;
 		col1 = remove_extra_spaces(col1);
-		std::cout << col1 << std::endl;
+		// std::cout << col1 << std::endl;
 		std::getline(sstream, cols, '|');
 		cols = remove_extra_spaces(cols);
-		// bool	fail;
-		std::cout << db.find(col1)->first <<std::endl; //<< ": " << itoa(db.find(col1)->second, fail) * itoa(cols, fail);
+		bool	fail;
+		
+		database::iterator value_in_db = db.find(col1);
+		if (value_in_db != db.end())
+			std::cout << db.find(col1)->first << ": s" << db.find(col1)->second << ": " << itoa(db.find(col1)->second, fail) * itoa(cols, fail) << std::endl;
 	}
 }
