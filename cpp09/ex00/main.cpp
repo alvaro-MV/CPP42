4c3cc23b5139dcade23a392c358148ca439f436d
Álvaro Morales <moralesalvaro97@gmail.com>
2025-07-04 20:17:21 +0200

    fill_db function created for managing both data.csv and input.txt

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -4,31 +4,12 @@
 #include <string>
 #include <map>
 
-typedef std::map<std::string, std::string> database;
+typedef std::string str;
+typedef std::map<str, str> database;
 
-void	fill_database(database &db) {
-	std::string	row;
-	std::fstream fin;
-	
-	fin.open("data.csv", std::ios::in);
-	
-	std::getline(fin, row);
-	while (std::getline(fin, row)) {
-		std::string cols;
-		std::stringstream sstream(row);
-
-		std::getline(sstream, cols, ',');
-		std::string col1 = cols;
-		std::getline(sstream, cols, ',');
-		db.insert(std::pair<std::string, std::string>(col1, cols));
-		// std::cout << db.find(col1)->first << ": " << db.find(col1)->second << std::endl;
-	}
-	std::cout << "Este es la de 2022-12-22: " << db.find("2021-12-22")->second << std::endl;
-}
-
-std::string remove_extra_spaces(const std::string& input) {
+str remove_extra_spaces(const str& input) {
     std::stringstream ss(input);
-    std::string word, result;
+    str word, result;
 
     while (ss >> word) {
         if (!result.empty())
@@ -39,7 +20,37 @@ std::string remove_extra_spaces(const std::string& input) {
     return result;
 }
 
-int	itoa(std::string str, bool &fail) {
+str	identity(const str &input) {
+	str iden = input;
+	return (iden);
+}
+
+void	fill_db(database &db, const char *file, char del, str (*transform)(const str&)) {
+	str	row;
+	std::fstream fin;
+	fin.open(file, std::ios::in);
+	
+	std::getline(fin, row);
+	while (std::getline(fin, row)) {
+		str cols;
+		std::stringstream sstream(row);
+
+		std::getline(sstream, cols, del);
+		str col1 = cols;
+		col1 = transform(col1);
+		std::cout << col1 << std::endl;
+		std::getline(sstream, cols, del);
+		cols = transform(cols);
+		db.insert(std::pair<str, str>(col1, cols));
+		// bool	fail;
+		
+		// database::iterator value_in_db = db.find(col1);
+		// if (value_in_db != db.end())
+		// 	std::cout << db.find(col1)->first << ": s" << db.find(col1)->second << ": " << itoa(db.find(col1)->second, fail) * itoa(cols, fail) << std::endl;
+	}
+}
+
+int	itoa(str str, bool &fail) {
 	int num;
 	std::istringstream iss(str);
 
@@ -59,28 +70,10 @@ int	itoa(std::string str, bool &fail) {
 int	main(void) {
 
 	database db;
-	fill_database(db);
-	std::cout << "Por segunda vez de 2022-12-22: " << db.find("2021-12-22")->second << std::endl;
+	fill_db(db, "data.csv", ',', identity);
+	// std::cout << "Por segunda vez de 2022-12-22: " << db.find("2021-12-22")->second << std::endl;
 
-	std::string	row;
-	std::fstream fin;
-	fin.open("input.txt", std::ios::in);
-	
-	std::getline(fin, row);
-	while (std::getline(fin, row)) {
-		std::string cols;
-		std::stringstream sstream(row);
-
-		std::getline(sstream, cols, '|');
-		std::string col1 = cols;
-		col1 = remove_extra_spaces(col1);
-		// std::cout << col1 << std::endl;
-		std::getline(sstream, cols, '|');
-		cols = remove_extra_spaces(cols);
-		bool	fail;
-		
-		database::iterator value_in_db = db.find(col1);
-		if (value_in_db != db.end())
-			std::cout << db.find(col1)->first << ": s" << db.find(col1)->second << ": " << itoa(db.find(col1)->second, fail) * itoa(cols, fail) << std::endl;
-	}
+	database input;
+	fill_db(input, "input.txt", '|', remove_extra_spaces);
+	std::cout << db.find("2011-01-09")->first << ": s";// << db.find("2011-01-09")->second << ": " << itoa(db.find(col1)->second, fail) * itoa(, fail) << std::endl;
 }
